2268bf5f9ad8820c1ea71cb86d3c10c0f62fbe21
Andrea Marini <andrea.marini@cern.ch>
2014-01-01 12:36:51 +0100

    500 jobs for mc

--- test/submitMC.sh
+++ test/submitMC.sh
@@ -20,14 +20,14 @@ rm -v log/logMC_*.log || true
 rm -v log/logMC_*.done || true
 rm -v log/logMC_*.fail || true
 
-for i in `seq 0 100` ; do
+for i in `seq 0 500` ; do
 bsub -q 1nd -J Job_MC_$i -o $PWD/log/logMC_$i.log <<EOF
 cd $PWD
 export SCRAM_ARCH=slc5_amd64_gcc462
 eval \`scramv1 runtime -sh\`
 echo "*** MC ***"
 rm $PWD/log/logMC_$i.done || true
-python python/step1_makeHisto.py --inputDat=data/configMC.dat --nJobs=100 --jobId=$i 2>&1 | gzip >$PWD/log/logMC_$i.txt.gz ;
+python python/step1_makeHisto.py --inputDat=data/configMC.dat --nJobs=500 --jobId=$i 2>&1 | gzip >$PWD/log/logMC_$i.txt.gz ;
 declare -a PS=${PIPESTATUS[@]} ;
 [ "${PS[0]}" == "0" ] && touch $PWD/log/logMC_$i.done || { touch $PWD/log/logMC_$i.fail; exit 1; }
 
